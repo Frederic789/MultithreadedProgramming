f10e5dc956080841d6f22e761c8f5839f81fee42
Freddy <fredericdouamba2014@gmail.com>
2023-10-23 15:00:34 -0700

    Initial Commit

---
+++ MultithreadedProgramming/MultithreadedProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultithreadedProgrammingApp.swift
+//  MultithreadedProgramming
+//
+//  Created by Student Account on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MultithreadedProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

59cec83f1de0268059d7c8f5298d2cafa829dc6c
Freddy <fredericdouamba2014@gmail.com>
2023-10-23 21:15:44 -0700

    Grand Central Dispatch

--- MultithreadedProgramming/ContentView.swift
+++ MultithreadedProgramming/ContentView.swift
@@ -6,19 +6,49 @@
 //
 
 import SwiftUI
-
+import Dispatch
 struct ContentView: View {
+    @State private var taskStatus = "Ready to start long task!"
+    @State private var isWorking = false
+    @State private var sliderValue: Double = 50
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Button(action: startLongTask) {
+                Text("Start Long Task")
+            }
+            .disabled(isWorking)
+            
+            Text(taskStatus)
+            
+            Slider(value: $sliderValue, in: 0...100, step: 1.0)
+            Text("Slider value: \(Int(sliderValue))")
         }
         .padding()
     }
+    
+    func startLongTask() {
+        taskStatus = "Starting the long task..."
+        isWorking = true
+        
+       
+        DispatchQueue.global(qos: .background).async {
+           
+            Thread.sleep(forTimeInterval: 10)
+            
+          
+            DispatchQueue.main.async {
+                self.taskStatus = "Long task completed!"
+                self.isWorking = false
+            }
+        }
+    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+
